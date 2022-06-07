b05712b205240d8b4287794aedbdd5eebb520699
YOUNGSHAYAN <92734199+youngshayan@users.noreply.github.com>
2022-06-07 15:47:22 +0430

    Add files via upload

---
+++ L5/Part one/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Enter your informations:"); 
+        User u=new 
+        User("a2","shayan",1234,20,"karaj",912345678,"diploma");
+        User u2=new User();
+        System.out.println(u.username); 
+        System.out.println(u.realname); 
+        System.out.println(u.password); 
+        System.out.println(u.age); 
+        System.out.println(u.address); 
+        System.out.println(u.phonenumber); 
+        
+        System.out.println(u.education);	 }}
---
+++ L5/Part one/User.java
@@ -0,0 +1,18 @@
+
+    public class User 
+{ public String username;
+    
+    public String realname;
+    public int password; 
+    public int age;
+    public String address;
+    public long phonenumber; 
+    public String educatipubl
+    public User(String username,String realname,int password,int age,String address,long phonenumber,String education)
+    { this.username=username; this.realname=realname; this.password=password;
+        this.age=age; this.address=address; this.phonenumber=phonenumber;
+        
+        this.education=education; } public User(){};
+
+    
+}
